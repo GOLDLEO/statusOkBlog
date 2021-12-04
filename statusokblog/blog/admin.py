aad3170de737d4f680b672bd5870b3ea8f4f2a83
GOLDLEO <oceanven@gmail.com>
2021-12-04 20:51:07 +0200

    Merge with branch ivan

--- statusokblog/blog/admin.py
+++ statusokblog/blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Post, Tag
+from .models import Post, Tag, Comment
 
 admin.site.register(Post)
 admin.site.register(Tag)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created')
+    search_fields = ('user_name', 'email', 'comment_text')
+
+
+admin.site.register(Comment, CommentAdmin)
